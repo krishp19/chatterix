36f1ef444a020fdefdf6184d1cd6c3e6492f5923
Krish Prasad <19krishprasad06@gmail.com>
2024-08-07 10:24:15 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoute from "./routes/authRoute.js"
 import messageRoute from "./routes/messageRoute.js"
 import userRoute from "./routes/userRoute.js"
+
 import {app, server} from './socket/socket.js'
 
 import { connectDb } from "./db/connectDb.js";
 
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -20,10 +25,15 @@ app.use("/api/auth",authRoute)
 app.use("/api/messages",messageRoute)
 app.use("/api/user",userRoute)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 // app.get("/", (req,res) => {
 //     res.send("Hello World!");
 // })
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
